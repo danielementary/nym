8d6bdebdfba8e5ba470f933fe3be9a9f376f9653
Daniel Filipe Nunes Silva <danielementary@gmail.com>
2021-12-27 17:58:13 +0100

    add strut for vouchers and paramaters and new function for parameters

--- common/nymcash/src/lib.rs
+++ common/nymcash/src/lib.rs
@@ -1,7 +1,46 @@
+use nymcoconut::{CoconutError, Parameters};
+
+use bls12_381::Scalar;
+
+pub struct ECashParams {
+    pub coconut_params: Parameters,
+    pub pay_max: Scalar,
+    pub voucher_max: Scalar,
+}
+
+impl ECashParams {
+    pub fn new(
+        num_attributes: u32,
+        pay_max: Scalar,
+        voucher_max: Scalar,
+    ) -> Result<ECashParams, CoconutError> {
+        Ok(ECashParams {
+            coconut_params: Parameters::new(num_attributes)?,
+            pay_max,
+            voucher_max,
+        })
+    }
+}
+
+pub struct Voucher {
+    pub binding_number: Scalar,
+    pub value: Scalar,
+    pub serial_number: Scalar,
+    pub info: Scalar,
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn main() {
-        assert_eq!(3, 4);
+    fn main() -> Result<(), CoconutError> {
+        let num_attributes = 4;
+        let pay_max = Scalar::from(10);
+        let voucher_max = Scalar::from(10);
+
+        let params = ECashParams::new(num_attributes, pay_max, voucher_max)?;
+
+        Ok(())
     }
 }

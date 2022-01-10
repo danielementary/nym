4221036705ee31a0d9c5862dd07a3925b1242de9
Daniel Filipe Nunes Silva <danielementary@gmail.com>
2022-01-10 18:51:54 +0100

    add ECashParams struct and corresponding new function

--- common/nymcash/src/lib.rs
+++ common/nymcash/src/lib.rs
@@ -1,8 +1,22 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use bls12_381::Scalar;
+use nymcoconut::{CoconutError, Parameters};
+
+pub struct ECashParams {
+    pub coconut_params: Parameters,
+    pub pay_max: Scalar,
+    pub voucher_max: Scalar,
+}
+
+impl ECashParams {
+    pub fn new(
+        num_attributes: u32,
+        pay_max: Scalar,
+        voucher_max: Scalar,
+    ) -> Result<ECashParams, CoconutError> {
+        Ok(ECashParams {
+            coconut_params: Parameters::new(num_attributes)?,
+            pay_max,
+            voucher_max,
+        })
     }
 }

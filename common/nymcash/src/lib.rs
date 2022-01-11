f83b7880141ec5a8386f063e8cd2c7fc88940f41
Daniel Filipe Nunes Silva <danielementary@gmail.com>
2022-01-11 09:09:31 +0100

    add Voucher struct and methods to create and access attributes

--- common/nymcash/src/lib.rs
+++ common/nymcash/src/lib.rs
@@ -21,6 +21,57 @@ impl ECashParams {
     }
 }
 
+#[derive(Debug, Copy, Clone)]
+pub struct Voucher {
+    pub binding_number: Scalar,
+    pub value: Scalar,
+    pub serial_number: Scalar,
+    pub info: Scalar,
+}
+
+impl Voucher {
+    pub fn new(coconut_params: &Parameters, binding_number: Scalar, value: Scalar) -> Voucher {
+        Voucher {
+            binding_number,
+            value,
+            serial_number: coconut_params.random_scalar(),
+            info: Scalar::from(0),
+        }
+    }
+
+    pub fn new_many(
+        coconut_params: &Parameters,
+        binding_number: Scalar,
+        values: &[Scalar],
+    ) -> Vec<Voucher> {
+        values
+            .iter()
+            .map(|v| Voucher::new(coconut_params, binding_number, *v))
+            .collect()
+    }
+
+    pub fn private_attributes(&self) -> Vec<Scalar> {
+        vec![self.binding_number, self.value, self.serial_number]
+    }
+
+    pub fn public_attributes(&self) -> Vec<Scalar> {
+        vec![self.info]
+    }
+
+    pub fn attributes(&self) -> Vec<Scalar> {
+        vec![
+            self.binding_number,
+            self.value,
+            self.serial_number,
+            self.info,
+        ]
+    }
+
+    pub fn number_of_attributes() -> u32 {
+        4
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -28,7 +79,7 @@ mod tests {
     #[test]
     fn main() -> Result<(), CoconutError> {
         // define e-cash parameters
-        let num_attributes = 4;
+        let num_attributes = Voucher::number_of_attributes();
         let pay_max = Scalar::from(10);
         let voucher_max = Scalar::from(10);
 
